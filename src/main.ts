815c0174e5907f96fccf9aaf5d88bc2a53e7c0dd
ucanekmek <onrerngkbudak@hotmail.com>
2025-10-21 12:56:02 +0300

    Küçük Eklemeler

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import AppHead from './component/Header/AppHead.vue'
+import { SpeedInsights } from "@vercel/speed-insights/vue"
 
 const app = createApp(App)
 

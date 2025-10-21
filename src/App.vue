87be6a9889c961079ef598e9e1bbf9fdf6efab93
ucanekmek <onrerngkbudak@hotmail.com>
2025-10-21 18:50:35 +0300

    Küçük Düzeltmeler, Mobil Uyumluluk ve Gereksiz Dosya Temizliği

--- src/App.vue
+++ src/App.vue
@@ -18,10 +18,10 @@ const todo = addTodo();
       <router-view />
   </main>
 
-  <footer class="bg-[rgb(40,40,40)] text-white text-center p-4 flex flex-wrap gap-2 justify-between">
-    <div class="flex gap-1 bg-white text-black rounded-2xl pr-2" >
+  <footer class="bg-[rgb(40,40,40)] text-white text-center p-4 flex flex-wrap gap-2 justify-center sm:justify-between">
+    <div class="gap-1 bg-white text-black rounded-2xl hidden sm:flex" >
       <img src="https://cdn.discordapp.com/avatars/685595486560059489/a_6a0ed6e07fe0a0b33217d95259be8900.gif?size=512" alt="İnternet Bağlantını Kontrol Et" class="w-6 h-6 rounded-3xl"  >
-      <h1 class="truncate w-24 sm:w-auto" >ucanekmek Tarafından Kodlanmıştır.</h1>
+      <h1>ucanekmek Tarafından Kodlanmıştır.</h1>
     </div>
 
     <h1 class="flex gap-1 bg-white text-black rounded-2xl pr-2 pl-2 font-semibold" >Toplamda {{ todo.storage.length }} Adet Todonuz Var</h1>
--- src/page/HomePage.vue
+++ src/page/HomePage.vue
@@ -8,15 +8,18 @@ const todo = addTodo();
 
 <template>
   <div v-if="todo.storage.length === 0" class="w-full h-[80vh]  text-4xl flex justify-center items-center text-white flex-col" >
-    <DotLottieVue  style="height: 390px; width: 390px"  :loop="false" autoplay src="https://lottie.host/f34ecb99-0044-499e-be22-bcaa85809ea9/wkmXT1I5N1.lottie" />
+    <DotLottieVue  style="height: 342px; width: 342px"  :loop="false" autoplay src="https://lottie.host/f34ecb99-0044-499e-be22-bcaa85809ea9/wkmXT1I5N1.lottie" />
     <h1 class="select-none" >Burası Boş Galiba</h1>
   </div>
 
   <div class="flex flex-col gap-2 m-2" v-else>
     <div v-for="data in todo.storage" :key="data.id" class="w-full bg-gray-500 rounded-2xl p-3 font-semibold break-words flex flex-col gap-1" >
       <h1 class="select-none" >{{ data.txt }}</h1>
-      <p class="select-none w-64 pr-2 pl-2 rounded-2xl bg-white" >{{ data.time }} Tarihinde Yazıldı</p>
-    </div>
 
+      <div class="flex select-none w-33 sm:w-64 pr-2 pl-2 rounded-2xl bg-white gap-2" >
+          <p class="" >{{ data.time }}</p>
+          <p class="hidden sm:flex" >Tarihinde Yazıldı</p>
+      </div>
+    </div>
   </div>
 </template>
